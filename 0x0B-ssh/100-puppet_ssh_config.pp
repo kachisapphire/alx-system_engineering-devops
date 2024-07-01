0252ea637b176a35e5003306cb439af6612e15f4
kachisapphire <mychaelesther99@gmail.com>
2024-07-01 13:10:11 -0700

    puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#ascriot that connects to server without passkey
+file {'/etc/ssh/ssh_config':
+  ensure  => 'present',
+}
+file_line {'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  match   => 'PasswordAuthentication yes',
+  replace => 'true',
+}
+file_line {'Declare identity file':
+  ensure => 'present',
+  line   => 'IdentifyFile ~/.ssh/school',
+  match  => '^IdentifyFile',
+  path   => '/etc/ssh/ssh_config',
+}
