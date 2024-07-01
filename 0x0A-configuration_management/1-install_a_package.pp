f00e75df159578a3aa784ef0734db11037c66267
kachisapphire <mychaelesther99@gmail.com>
2024-07-01 06:34:11 -0700

    task1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,15 @@
 #Using Puppet, install flask from pip3.
-package {'flask':
+package {'Python':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
+
+package {'Flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package {'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
