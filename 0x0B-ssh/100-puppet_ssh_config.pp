3d9599710e0914a1e00706271dc53a60210650d5
kachisapphire <mychaelesther99@gmail.com>
2024-07-01 13:13:27 -0700

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -10,8 +10,8 @@ file_line {'Turn off passwd auth':
   replace => 'true',
 }
 file_line {'Declare identity file':
-  ensure => 'present',
   line   => 'IdentifyFile ~/.ssh/school',
   match  => '^IdentifyFile',
   path   => '/etc/ssh/ssh_config',
+  ensure => 'present',
 }
